9771d9d07890e29f6b6842a1747bcd432172fe26
Your Name <you@example.com>
2024-08-16 20:52:26 +0500

    updated prompt template

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ class GraphState(TypedDict):
     
 
 
-st.set_page_config(page_title='AI Market Researcher', page_icon="books")
+st.set_page_config(page_title='AI Market Researcher', page_icon="👨🏻‍💻")
 
 
 # Define the nodes
